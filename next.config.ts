d45b864f4b0b5981587cefaba22af83ee4a38742
Yurii-Masniuk <yeristar@gmail.com>
2025-11-09 16:29:34 +0200

    change back next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	output: 'export',
-	basePath: '/08-zustand',
 	images: {
 		remotePatterns: [
 			{ protocol: 'https', hostname: 'ac.goit.global' }
